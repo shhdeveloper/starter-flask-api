0d2c34b1d0632691b385ffe6ba819f063bade96b
shhdeveloper <shhdeveloper@gmail.com>
2023-10-25 14:18:44 +1030

    s

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask
-import requests
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def make_get_request():
-    url = "https://dark-pink-indri-yoke.cyclic.app/add"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        response_text = response.text
-    else:
-        response_text = "Failed to retrieve data from " + url
+my_list = []  # Initialize a list to store data
 
-    return response_text
+@app.route('/')
+def index():
+    # Render the HTML template
+    return render_template('index.html', my_list=my_list)
+
+@app.route('/add', methods=['POST'])
+def add_data():
+    new_data = request.form.get('data')
+    my_list.append(new_data)
+    return render_template('index.html', my_list=my_list)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
