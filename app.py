03315e4b0a0472d134b4ff786d6ac35b3efe42e1
shhdeveloper <shhdeveloper@gmail.com>
2023-10-25 13:59:50 +1030

    s

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!123'
-socketio = SocketIO(app, cors_allowed_origins="*")
-
-@socketio.on('message')
-def handle_message(data):
-    message = data.get('message', '')
-    username = data.get('username', '')
-
-    print("Received message from", username, ":", message)
-    if message != "User connected!":
-        send({'message': message, 'username': username}, broadcast=True)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+
+def hello_world():
+    # Create an example list
+    my_list = ["String 1", "String 2", "String 3"]
+
+    # Return the list as a JSON response
+    return jsonify(my_list)
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
