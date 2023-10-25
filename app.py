d35eb24d1d83b193fdde6baf6457edc382bcd33d
shhdeveloper <shhdeveloper@gmail.com>
2023-10-25 14:23:16 +1030

    s

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
-my_list = []  # Initialize a list to store data
-
 @app.route('/')
-def index():
-    # Render the HTML template
-    return render_template('index.html', my_list=my_list)
 
-@app.route('/add', methods=['POST'])
-def add_data():
-    new_data = request.form.get('data')
-    my_list.append(new_data)
-    return render_template('index.html', my_list=my_list)
+def hello_world():
+    # Create an example list
+    my_list = ["String 1", "String 2", "String 3"]
+
+    # Return the list as a JSON response
+    return jsonify(my_list)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+    
+
+
