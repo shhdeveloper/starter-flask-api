e14973601a6b7fd55311e8238c67c6c76f6c14b7
shhdeveloper <shhdeveloper@gmail.com>
2023-10-25 15:11:44 +1030

    p

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import asyncio
 from flask import Flask, jsonify, render_template, request
 import os
 import subprocess
+from ping3 import ping, verbose_ping
 
 # Define the host or IP address you want to ping
 
@@ -13,27 +14,26 @@ my_list = []  # Initialize a list to store data
 
 async def ping_host(host):
     try:
-        result = subprocess.check_output(["ping", "-c", "1", host])
-        result = result.decode("utf-8")  # Decode the binary output to a string
-        return result
+        ping_time = ping(host)
+        return ping_time
     except subprocess.CalledProcessError:
         return "Ping failed. The host may be unreachable."
 
 @app.route('/add_data', methods=['POST'])
 def add_data():
-    host = "google.com"  # Change this to the desired host
+    host = "cyclic.sh"  # Change this to the desired host
     data = request.get_json()  # Assuming you send JSON data in the request
 
     if 'data' in data:
         new_data = data['data']
 
         # Asynchronously ping the host
-        #ping_result = asyncio.run(ping_host(host))
+        ping_result = asyncio.run(ping_host(host))
 
         my_list.append({"data": new_data})
-        return jsonify({"message": "Data added successfully"})
+        return jsonify({"message": "Data added successfully", "ping": ping_result})
     else:
-        return jsonify({"error": "Invalid data format"}), 400
+        return jsonify({"error": "Invalid data format", "ping": ping_result}), 400
 
 @app.route('/get_data', methods=['GET'])
 def get_data():
