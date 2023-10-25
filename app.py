958f5dc0d1911fb92acb267c95b7991d7484b676
shhdeveloper <shhdeveloper@gmail.com>
2023-10-25 13:42:01 +1030

    s

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, world!'
+
+# Create an empty list
+my_list = []
+
+# Add string data to the list
+my_list.append("String 1")
+
+# Print the list
+print(my_list)
