123c144ce7dc73c3b4479904754e6504342e429b
shhdeveloper <shhdeveloper@gmail.com>
2023-10-25 23:58:12 +1030

    s

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
-# from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# my_list = []  # Initialize a list to store data
+my_list = []  # Initialize a list to store data
 
-# @app.route('/add_data', methods=['POST'])
-# def add_data():
-#     data = request.get_json()  # Assuming you send JSON data in the request
+@app.route('/add_data', methods=['POST'])
+def add_data():
+    data = request.get_json()  # Assuming you send JSON data in the request
 
-#     if 'data' in data:
-#         new_data = data['data']
-#         my_list.append(new_data)
-#         return jsonify({"message": "Data added successfully"})
-#     else:
-#         return jsonify({"error": "Invalid data format"}), 400
+    if 'data' in data:
+        new_data = data['data']
+        my_list.append(new_data)
+        return jsonify({"message": "Data added successfully"})
+    else:
+        return jsonify({"error": "Invalid data format"}), 400
 
-# @app.route('/get_data', methods=['GET'])
-# def get_data():
-#     return jsonify({"data": my_list})
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    return jsonify({"data": my_list})
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
