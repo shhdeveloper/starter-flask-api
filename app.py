805beef40ccf1d9f09a319d54cac58473208187d
shhdeveloper <shhdeveloper@gmail.com>
2023-10-25 14:17:48 +1030

    s

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, request, jsonify
-
+from flask import Flask
 import requests
 
-url = "https://dark-pink-indri-yoke.cyclic.app/add"
+app = Flask(__name__)
 
-response = requests.get(url)
+@app.route('/')
+def make_get_request():
+    url = "https://dark-pink-indri-yoke.cyclic.app/add"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        response_text = response.text
+    else:
+        response_text = "Failed to retrieve data from " + url
 
-if response.status_code == 200:
-    print("Response from", url, ":", response.text)
-else:
-    print("Failed to retrieve data from", url)
-
+    return response_text
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
